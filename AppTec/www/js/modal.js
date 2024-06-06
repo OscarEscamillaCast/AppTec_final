d9517b980569b4443f732b4208b515c30168ed28
UrielCG1 <l20140185@queretaro.tecnm.mx>
2024-06-05 21:53:13 -0600

    confirmacion

--- AppTec/www/js/modal.js
+++ AppTec/www/js/modal.js
@@ -1,17 +1,16 @@
 function mostrarModal() {
-    document.getElementById("myModal").style.display = "block";
+  var modal = document.getElementById("myModal");
+  modal.classList.add("show");
+}
+
+function cerrarModal() {
+  var modal = document.getElementById("myModal");
+  modal.classList.remove("show");
+}
+
+window.onclick = function(event) {
+  var modal = document.getElementById("myModal");
+  if (event.target == modal) {
+      modal.classList.remove("show");
   }
-  
-  
-    function cerrarModal() {
-        var modal = document.getElementById("myModal");
-        modal.style.display = "none"; // Ocultar el modal al hacer clic en el botón de cierre
-      }
-  
-  
-  window.onclick = function(event) {
-    if (event.target == document.getElementById("myModal")) {
-      document.getElementById("myModal").style.display = "none";
-    }
-  }
-  
+}
